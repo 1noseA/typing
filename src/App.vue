ed2dcc061779bb1654fe4cf1925a198674f87d07
1noseA <1noseA2L@gmail.com>
2024-05-04 17:37:42 +0900

    問題を追加

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <h1>Typing Game</h1>
       <div class="marker"></div>
     </div>
-    <button class="start-button">START</button>
+    <button class="start-button mb-20">START</button>
+    <div class="question mb-20">apple</div>
+    <div class="clear">Clear!</div>
   </div>
 </template>
 
