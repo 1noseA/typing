1f7bef911600204ff80c4a30b2a4cc2427224552
1noseA <1noseA2L@gmail.com>
2024-05-04 22:42:35 +0900

    正答数カウントを追加

--- src/components/TypingPage.vue
+++ src/components/TypingPage.vue
@@ -15,7 +15,7 @@
       <div class="gauge-wrapper mb-20">
         <div class="gauge"></div>
       </div>
-      <div>1/5</div>
+      <div>{{ current_question_counts }}/{{ question_counts }}</div>
     </div>
   </div>
 </template>
@@ -34,7 +34,9 @@ export default {
         'kiwi',
         'melon',
       ],
-      typeBox: ''
+      typeBox: '',
+      current_question_counts: 0,
+      question_counts: 0
     }
   },
   methods: {
@@ -42,8 +44,10 @@ export default {
       this.startFlg = true;
     }
   },
+  // 描画されたタイミング
   mounted: function() {
     this.current_question = this.questions[0]
+    this.question_counts = this.questions.length
   },
   watch: {
     typeBox: function(e) {
@@ -51,6 +55,7 @@ export default {
         this.questions.splice(0,1)
         this.current_question = this.questions[0]
         this.typeBox = ''
+        this.current_question_counts = this.current_question_counts + 1
       }
     }
   }
