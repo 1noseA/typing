6d2f4e267fca195daaf6fa57cd064c29b139fbca
1noseA <1noseA2L@gmail.com>
2024-05-04 22:20:05 +0900

    表示制御を追加

--- src/components/TypingPage.vue
+++ src/components/TypingPage.vue
@@ -5,16 +5,18 @@
       <div class="marker"></div>
     </div>
     <button v-if="startFlg!=true" class="start-button mb-20" @click="gameStart">START</button>
-    <div class="question mb-20">apple</div>
-    <div class="clear">Clear!</div>
-    <div class="type-form-wrapper mb-20">
-      <input type="text" class="type-form">
-    </div>
+    <div v-if="startFlg">
+      <div class="question mb-20">apple</div>
+      <div class="clear">Clear!</div>
+      <div class="type-form-wrapper mb-20">
+        <input type="text" class="type-form">
+      </div>
 
-    <div class="gauge-wrapper mb-20">
-      <div class="gauge"></div>
+      <div class="gauge-wrapper mb-20">
+        <div class="gauge"></div>
+      </div>
+      <div>1/5</div>
     </div>
-    <div>1/5</div>
   </div>
 </template>
 
