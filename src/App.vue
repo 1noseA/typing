1aef5214c028d69fc7022f88db4479af8f0ac140
1noseA <1noseA2L@gmail.com>
2024-05-04 17:49:33 +0900

    ゲージとカウンタを追加

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,14 @@
     <button class="start-button mb-20">START</button>
     <div class="question mb-20">apple</div>
     <div class="clear">Clear!</div>
-    <div class="type-form-wrapper">
+    <div class="type-form-wrapper mb-20">
       <input type="text" class="type-form">
     </div>
+
+    <div class="gauge-wrapper mb-20">
+      <div class="gauge"></div>
+    </div>
+    <div>1/5</div>
   </div>
 </template>
 
