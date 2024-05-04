e339161d6ddb744ebf1b5f28b690b39271853f64
1noseA <1noseA2L@gmail.com>
2024-05-04 22:17:02 +0900

    scriptを修正

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
-  <div class="container">
-    <div class="title">
-      <h1>Typing Game</h1>
-      <div class="marker"></div>
-    </div>
-    <button class="start-button mb-20">START</button>
-    <div class="question mb-20">apple</div>
-    <div class="clear">Clear!</div>
-    <div class="type-form-wrapper mb-20">
-      <input type="text" class="type-form">
-    </div>
-
-    <div class="gauge-wrapper mb-20">
-      <div class="gauge"></div>
-    </div>
-    <div>1/5</div>
+  <div>
+    <TypingPage />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import TypingPage from './components/TypingPage.vue'
 
-// export default {
-//   name: 'App',
-//   components: {
-//     HelloWorld
-//   }
-// }
+export default {
+  name: 'App',
+  components: {
+    TypingPage
+  }
+}
 </script>
-
-<style src="./assets/style.css" scoped></style>
---
+++ src/components/TypingPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="title">
+      <h1>Typing Game</h1>
+      <div class="marker"></div>
+    </div>
+    <button v-if="startFlg!=true" class="start-button mb-20" @click="gameStart">START</button>
+    <div class="question mb-20">apple</div>
+    <div class="clear">Clear!</div>
+    <div class="type-form-wrapper mb-20">
+      <input type="text" class="type-form">
+    </div>
+
+    <div class="gauge-wrapper mb-20">
+      <div class="gauge"></div>
+    </div>
+    <div>1/5</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TypingPage',
+  data() {
+    return {
+      startFlg: ''
+    }
+  },
+  methods: {
+    gameStart: function() {
+      this.startFlg = true;
+    }
+  }
+}
+</script>
+
+<style src="../assets/style.css" scoped></style>
